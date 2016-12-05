bc053991071ea7e0a879416efef54b0128bc3c96
qqbuby <qqbuby@gmail.com>
2016-12-05 03:29:17 -0800

    Add Dockerfile

---
+++ datas/local_settings/base.py
@@ -0,0 +1,6 @@
+# PRODUCTION_DOMAIN = 'readthedocs.com'
+# SLUMBER_API_HOST = 'http://readthedocs.com'
+# PUBLIC_API_URL = 'http://readthedocs.com'
+
+TIME_ZONE = 'Asia/Chongqing'
+
---
+++ datas/local_settings/pro.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = False
+ALLOW_ADMIN = False
+
